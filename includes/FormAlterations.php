ed9f1f02931adbd9de79b95a31adf5f10f26e34b
Stephen Groundwater <stephe.groundwater@gmail.com>
2024-04-22 20:01:40 -0400

    True-up of install code.

--- includes/FormAlterations.php
+++ includes/FormAlterations.php
@@ -1,7 +1,8 @@
 <?php
 
+// Import the FormStateInterface.
+use Drupal\Core\Form\FormStateInterface;
+
 function rsvp_system_handle_form_alter(&$form, FormStateInterface $form_state, $form_id) {
-    // Logic to add custom validations, settings, or AJAX functionality.
-    $form['#validate'][] = 'rsvp_system_form_validate';
-    // Example: add AJAX to a field.
+    // Function implementation
 }
