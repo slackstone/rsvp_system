6ba796c711fe1732f643f2469d174a897014e65c
Stephen Groundwater <stephe.groundwater@gmail.com>
2024-04-25 07:11:06 -0400

    Adding a copyright statement, and confirming the project is released under GPL 3.0. Enjoy.

--- includes/FormAlterations.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Import the FormStateInterface.
-use Drupal\Core\Form\FormStateInterface;
-
-function rsvp_system_handle_form_alter(&$form, FormStateInterface $form_state, $form_id) {
-    // Function implementation
-}
